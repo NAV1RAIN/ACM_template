c07cb1e5c221acc594bc32bafa003066e8e7daa2
NAV1RAIN <songziqi1998@gmail.com>
2019-07-19 19:50:05 +0800

    floyd

---
+++ floyd.cc
@@ -0,0 +1,9 @@
+for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(cost[i][j] > cost[i][k] + cost[k][j]) {
+                        cost[i][jj] = cost[i][k] + cost[k][j];
+                    }
+                }
+            }
+        }
